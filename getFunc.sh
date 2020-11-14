ea88d984e52bf998e129a23651133a80cd10f614
5IGI0 <5IGI0@protonmail.com>
2020-11-14 22:39:22 +0100

    📝 Update getFunc.sh

--- getFunc.sh
+++ getFunc.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo $(basename $(ls /usr/share/man/man3 | sort -R | tail -n 1)) | cut -d. -f1
+echo $(basename $(ls /usr/share/man/man3 | grep -e "[a-zA-Z0-9\_]" | sort -R | tail -n 1)) | cut -d. -f1
