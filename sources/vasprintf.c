b5f7050674bc23381d3e6fe25460c1646b4b91f6
5IGI0 <5IGI0@protonmail.com>
2020-11-14 01:21:05 +0100

    ✨ Add vasprintf

---
+++ sources/vasprintf.c
@@ -0,0 +1,26 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdarg.h>
+#include <time.h>
+#include <stdlib.h>
+
+int my_asprintf(char **out, char const *format, ...) {
+    va_list args;
+    va_start(args, format);
+    int returner = vasprintf(out, format, args);
+    va_end(args);
+    return returner;
+}
+
+int main(void) {
+    char *tmp = NULL;
+
+    if (my_asprintf(&tmp, "timestamp: %d", time(NULL)) >= 0) {
+        puts(tmp);
+    } else {
+        perror("my_asprintf");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
